7ad6be147bbb614ca5926f8135eeb33a7394c36c
Kirill Koval <brcyrill2@gmail.com>
2023-02-20 04:44:11 +0200

    now here is application logic only

--- src/main/java/com/kerrrusha/Main.java
+++ src/main/java/com/kerrrusha/Main.java
@@ -1,9 +1,9 @@
 package com.kerrrusha;
 
-import com.kerrrusha.model.ScheduleElement;
-import com.kerrrusha.model.ScheduleType;
+import com.kerrrusha.generating.PossibleAnswersCreatingUtil;
+import com.kerrrusha.generating.TaskToMoodleXmlConverter;
+import com.kerrrusha.model.MoodlePossibleAnswer;
 import com.kerrrusha.model.Task;
-import com.kerrrusha.solving.TaskSolver;
 
 import java.util.List;
 
@@ -12,41 +12,12 @@ import static com.kerrrusha.generating.TaskGeneratorUtil.generateAmountOfTasks;
 public class Main {
 
     public static void main(String[] args) {
-        solveExample();
-        System.out.println("####################");
-        generateAndSolveAmount(10);
-    }
+        List<Task> tasks = generateAmountOfTasks(10);
 
-    private static void generateAndSolveAmount(int amount) {
-        List<Task> tasks = generateAmountOfTasks(amount);
         for (Task task : tasks) {
-            showSolvingProcess(task);
+            List<MoodlePossibleAnswer> possibleAnswerList = PossibleAnswersCreatingUtil.create(task);
+            new TaskToMoodleXmlConverter(task, possibleAnswerList).createMoodleXmlFile();
         }
-    }
-
-    private static void solveExample() {
-        Task example = new Task(5,
-                1,
-                ScheduleType.MAX_SUM_ENDING_TIME,
-                List.of(
-                new ScheduleElement(4, 9, 26, 1),
-                new ScheduleElement(2, 14, 34, 2),
-                new ScheduleElement(3, 10, 33, 3),
-                new ScheduleElement(1, 14, 34, 1),
-                new ScheduleElement(5, 7, 44, 2)
-        ));
-
-        showSolvingProcess(example);
-    }
-
-    private static void showSolvingProcess(Task task) {
-        System.out.println(task);
-
-        TaskSolver taskSolver = new TaskSolver(task);
-        System.out.println("Solved schedule:");
-        System.out.println(taskSolver.solve());
-
-        System.out.println();
     }
 
 }
