dc1e374129d6143e1c44e42ae4ce6b79b30bbdda
Kirill Koval <brcyrill2@gmail.com>
2023-02-20 04:46:02 +0200

    added util

--- src/main/java/com/kerrrusha/generating/TaskGeneratorUtil.java
+++ src/main/java/com/kerrrusha/generating/TaskGeneratorUtil.java
@@ -3,6 +3,7 @@ package com.kerrrusha.generating;
 import com.kerrrusha.model.ScheduleElement;
 import com.kerrrusha.model.ScheduleType;
 import com.kerrrusha.model.Task;
+import com.kerrrusha.model.TaskCondition;
 
 import java.util.*;
 
@@ -13,6 +14,10 @@ import static java.util.stream.Collectors.toList;
 public class TaskGeneratorUtil {
 
     private static final int EXPECTED_TIME_VALUE_DUPLICATE_GROUPS_AMOUNT = 1;
+
+    private static final int AMOUNT_OF_POSSIBLE_ALT_ANSWER_COUNTS = 6;
+    private static final int ALT_ANSWER_COUNT_FROM_INCLUSIVE = 1;
+    private static final int ALT_ANSWER_COUNT_TO_EXCLUSIVE = 25;
 
     private static final int N_FROM_INCLUSIVE = 5;
     private static final int N_TO_EXCLUSIVE = 8;
@@ -29,23 +34,44 @@ public class TaskGeneratorUtil {
     private static final int U_TO_INCLUSIVE = 4;
 
     public static Task generateSingleTask() {
-        int n = getRandomInt(N_FROM_INCLUSIVE, N_TO_EXCLUSIVE);
-        int m = M_DEFAULT_VALUE;
+        final int n = getRandomInt(N_FROM_INCLUSIVE, N_TO_EXCLUSIVE);
+        final int m = M_DEFAULT_VALUE;
+
         ScheduleType scheduleType = getRandomScheduleType();
-        List<ScheduleElement> schedule = new ArrayList<>();
-        while(getTimeValueDuplicateGroupsAmount(schedule) != EXPECTED_TIME_VALUE_DUPLICATE_GROUPS_AMOUNT) {
+        List<ScheduleElement> schedule = generateSchedule(n);
+        List<Integer> possibleAltAnswerCountsExceptCorrectOne = generatePossibleAltAnswerCountsExceptCorrectOne();
+
+        TaskCondition taskCondition = new TaskCondition(n, m, scheduleType, schedule, possibleAltAnswerCountsExceptCorrectOne);
+        return new Task(taskCondition);
+    }
+
+    private static List<Integer> generatePossibleAltAnswerCountsExceptCorrectOne() {
+        Set<Integer> result = new HashSet<>();
+        for (int i = 1; i < AMOUNT_OF_POSSIBLE_ALT_ANSWER_COUNTS; i++) {
+            result.add(getRandomInt(ALT_ANSWER_COUNT_FROM_INCLUSIVE, ALT_ANSWER_COUNT_TO_EXCLUSIVE));
+            if (result.size() != i) {
+                i -= 1;
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    private static List<ScheduleElement> generateSchedule(int n) {
+        List<ScheduleElement> result = new ArrayList<>();
+        do {
+            result.clear();
             for (int i = 1; i <= n; i++) {
                 int t = getRandomInt(T_FROM_INCLUSIVE, T_TO_INCLUSIVE);
-                if (getTimeValueDuplicateGroupsAmount(schedule, t) > EXPECTED_TIME_VALUE_DUPLICATE_GROUPS_AMOUNT) {
+                if (getTimeValueDuplicateGroupsAmount(result, t) > EXPECTED_TIME_VALUE_DUPLICATE_GROUPS_AMOUNT) {
                     i -= 1;
                     continue;
                 }
                 int d = getRandomInt(D_FROM_INCLUSIVE, D_TO_EXCLUSIVE);
                 int u = getRandomInt(U_FROM_INCLUSIVE, U_TO_INCLUSIVE);
-                schedule.add(new ScheduleElement(i, t, d, u));
+                result.add(new ScheduleElement(i, t, d, u));
             }
-        }
-        return new Task(n, m, scheduleType, schedule);
+        } while (getTimeValueDuplicateGroupsAmount(result) != EXPECTED_TIME_VALUE_DUPLICATE_GROUPS_AMOUNT);
+        return result;
     }
 
     private static ScheduleType getRandomScheduleType() {
