9d97d868849c2348db8484ae9fb6c07155617534
Kirill Koval <brcyrill2@gmail.com>
2023-04-05 15:41:41 +0300

    now, main app logic goes here

--- src/main/java/com/kerrrusha/Main.java
+++ src/main/java/com/kerrrusha/Main.java
@@ -1,23 +1,30 @@
 package com.kerrrusha;
 
+import com.kerrrusha.config.ConfigReader;
 import com.kerrrusha.generating.PossibleAnswersCreator;
+import com.kerrrusha.generating.TaskGenerator;
 import com.kerrrusha.generating.TaskToMoodleXmlConverter;
 import com.kerrrusha.model.MoodlePossibleAnswer;
 import com.kerrrusha.model.Task;
 
 import java.util.List;
 
-import static com.kerrrusha.generating.TaskGeneratorUtil.generateAmountOfTasks;
+import static com.kerrrusha.util.ConverterUtil.toInt;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Task> tasks = generateAmountOfTasks(100);
+        int AMOUNT_OF_QUESTIONS_TO_GENERATE = toInt(new ConfigReader().getProperty("AMOUNT_OF_QUESTIONS_TO_GENERATE"));
 
+        List<Task> tasks = new TaskGenerator().generateAmountOfTasks(AMOUNT_OF_QUESTIONS_TO_GENERATE);
+
+        StringBuilder questionsXml = new StringBuilder();
         for (Task task : tasks) {
             List<MoodlePossibleAnswer> possibleAnswerList = new PossibleAnswersCreator(task).create();
-            new TaskToMoodleXmlConverter(task, possibleAnswerList).createMoodleXmlFile();
+            questionsXml.append(new TaskToMoodleXmlConverter(task, possibleAnswerList).getMoodleXmlQuestion()).append(System.lineSeparator());
         }
+
+        TaskToMoodleXmlConverter.createMoodleXmlFile(questionsXml.toString());
     }
 
 }
