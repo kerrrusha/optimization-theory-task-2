6ad60ae7f80aae55db6fec55065cee79d7b02f27
Kirill Koval <brcyrill2@gmail.com>
2023-04-05 15:38:59 +0300

    updated TaskNameGeneratorUtil to such format: "T2 LPT_F alt 1"

--- src/main/java/com/kerrrusha/generating/TaskNameGeneratorUtil.java
+++ src/main/java/com/kerrrusha/generating/TaskNameGeneratorUtil.java
@@ -3,18 +3,25 @@ package com.kerrrusha.generating;
 import com.kerrrusha.model.ScheduleType;
 import com.kerrrusha.model.Task;
 
+import static com.kerrrusha.model.MinOrMax.MIN;
+
 public class TaskNameGeneratorUtil {
 
-    private static final String FILENAME_TEMPLATE = "Т2-%s-%s-t";
+    private static final String FILENAME_TEMPLATE = "Т2 %s-%s alt %s";
 
     public static String generateTaskName(Task task) {
-        return String.format(FILENAME_TEMPLATE, task.getId(), decideMinOrMax(task.getTaskCondition().getScheduleType()));
+        return String.format(
+                FILENAME_TEMPLATE,
+                decideSptOrLpt(task.getTaskCondition().getScheduleType()),
+                task.getTaskCondition().getScheduleType().getShortCriterion(),
+                task.getId()
+        );
     }
 
-    private static String decideMinOrMax(ScheduleType scheduleType) {
-        return scheduleType.equals(ScheduleType.MIN_SUM_ENDING_TIME)
-                ? "min"
-                : "max";
+    private static String decideSptOrLpt(ScheduleType scheduleType) {
+        return scheduleType.getType().equals(MIN)
+                ? "SPT"
+                : "LPT";
     }
 
 }
